aabf549728b0cbe1a35cfc8c2f039b2c58beef71
barham7528 <kingbarham36@gmail.com>
2026-09-21 18:31:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ app.use(cors());
 app.use(express.json({ limit: "2mb" }));
 
 app.use(express.static("public"));
-
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/index.html");
+});
 app.get("/api/health", (req, res) => {
   res.json({
     ok: true,
